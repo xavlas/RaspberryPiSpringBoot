0b213431119f1c16885d478fa43cceccaeb95b3e
Shinjice <Tdna84@gmail.com>
2020-09-19 18:13:00 +0200

    first Commit

---
+++ src/main/java/RaspberryPiSpringBoot/RaspberryPiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package RaspberryPiSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryPiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberryPiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RaspberryPiSpringBoot/RaspberryPiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package RaspberryPiSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspberryPiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
